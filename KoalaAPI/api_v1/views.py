046d3ddff50d4f76bb2e6dc048e1e9d8721eaac3
bohan <bohanzhang@foxmail.com>
2015-08-06 17:30:58 +0800

    support get qrcode

--- KoalaAPI/api_v1/views.py
+++ KoalaAPI/api_v1/views.py
@@ -1,3 +1,4 @@
+import json
 import qrcode
 from StringIO import StringIO
 from flask import send_file, request
@@ -5,7 +6,7 @@ from flask import send_file, request
 from . import api_v1
 
 
-@api_v1.route('/qrcode', methods=['POST'])
+@api_v1.route('/qrcode')
 def v1_qrcode():
     box_size = request.args.get('box_size', 10)
     border = request.args.get('border', 4)
@@ -16,7 +17,10 @@ def v1_qrcode():
         box_size=box_size,
         border=border,
     )
-    qr.add_data(request.data)
+    if request.method == 'GET':
+        qr.add_data(json.dumps(request.args))
+    else:
+        qr.add_data(request.data)
     qr.make(fit=True)
 
     img = qr.make_image()
@@ -27,4 +31,4 @@ def _serve_pil_image(pil_img):
     img_io = StringIO()
     pil_img.save(img_io, 'PNG')
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='image/png', cache_timeout=0)
