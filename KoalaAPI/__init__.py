e3fa21a866adfd3e8a0f316df36d650e2f1699ab
bohan <bohanzhang@foxmail.com>
2015-10-09 22:58:27 +0800

    暂时硬编码敏感数据

--- KoalaAPI/__init__.py
+++ KoalaAPI/__init__.py
@@ -5,8 +5,8 @@ import os
 def create_app():
     app = Flask(__name__)
 
-    app.config['client_id'] = os.environ['client_id']
-    app.config['client_secret'] = os.environ['client_secret']
+    app.config['client_id'] = 'b36fd04ccb202122a5c7'
+    app.config['client_secret'] = '652be8dcb4bebbb5efec0828a2ef302816f0e006'
 
     from .api_v1 import api_v1 as api_v1_blueprint
     app.register_blueprint(api_v1_blueprint, url_prefix="/v1")
