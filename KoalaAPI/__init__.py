9fec9d5f168b591cd65f6e781c485b30b0311d5a
bohan <bohanzhang@foxmail.com>
2015-10-09 22:28:34 +0800

    github 登录测试

--- KoalaAPI/__init__.py
+++ KoalaAPI/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['client_id'] = os.environ['client_id']
+    app.config['client_secret'] = os.environ['client_secret']
 
     from .api_v1 import api_v1 as api_v1_blueprint
     app.register_blueprint(api_v1_blueprint, url_prefix="/v1")
--- KoalaAPI/views/tuchuang.py
+++ KoalaAPI/views/tuchuang.py
@@ -1,7 +1,10 @@
 from . import main_view
-from flask import render_template, request
+import json
+from os import abort
+from flask import render_template, request, url_for, current_app
 from qiniu import Auth, put_data
 import uuid
+import requests
 
 __author__ = 'bohan'
 
@@ -22,3 +25,25 @@ def tuchuang_index():
         url = 'http://%s.qiniudn.com/%s' % (bucket_name, key)
     return render_template('tuchuang.html', url=url, access_key=access_key, secret_key=secret_key,
                            bucket_name=bucket_name)
+
+
+@main_view.route('/tuchuang/callback')
+def tuchuang_callback():
+    code = request.args.get('code')
+    if not code:
+        abort(404)
+
+    url = 'https://github.com/login/oauth/access_token'
+    config = current_app.config
+    params = {
+        'client_id': config.get('client_id'),
+        'client_secret': config.get('client_secret'),
+        'code': code,
+        # 'redirect_uri': url_for('.tuchuang_index', _external=True)
+    }
+    result = requests.get(url, params, headers={'Accept': 'application/json'})
+    data = json.loads(result)
+    params = {"access_token": data.get('access_token')}
+    result = requests.get('https://api.github.com/user', params)
+    current_app.logger.debug('result: %s' % result)
+    return url_for('.tuchuang_index', result=result)
