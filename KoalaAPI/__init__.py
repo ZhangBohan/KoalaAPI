7240664a1c1e2d36fae979cc8037ba69a8cdf72e
bohan <bohanzhang@foxmail.com>
2015-10-10 11:11:53 +0800

    add secret key

--- KoalaAPI/__init__.py
+++ KoalaAPI/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-import os
+import logging
 
 
 def create_app():
     app = Flask(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+    app.logger.debug('creating app')
 
+    app.config['SECRET_KEY'] = 'alkjadsf,zxcnv,nmasroiuaewr'
     app.config['client_id'] = 'b36fd04ccb202122a5c7'
     app.config['client_secret'] = '652be8dcb4bebbb5efec0828a2ef302816f0e006'
 
